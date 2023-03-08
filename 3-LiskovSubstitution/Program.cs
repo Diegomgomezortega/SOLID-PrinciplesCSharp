a93fd7f68331109f5ca8138a06c23d9b2e504de4
Diego Martin <diegomartin_000@hotmail.com>
2023-03-07 23:33:42 -0300

    LiskovSubstitution

--- 3-LiskovSubstitution/Program.cs
+++ 3-LiskovSubstitution/Program.cs
@@ -1,16 +1,16 @@
 ﻿using Liskov;
 
-CalculateSalaryMonthly(new List<Employee>() {
+ShowSalaryMonthly(new List<Employee>() {
     new EmployeeFullTime("Pepito Pérez", 160, 10),
-    new EmployeeContractor("Manuel Lopera", 180, 0)
+    new EmployeeContractor("Manuel Lopera", 180)
 });
 
-void CalculateSalaryMonthly(List<Employee> employees) 
+void ShowSalaryMonthly(List<Employee> employees) 
 {
-    foreach (var item in employees)
+    foreach (var employee in employees)
     {
-        decimal salary = item.CalculateSalary((item is EmployeeFullTime));
-        Console.WriteLine($"The {item.Fullname}'s salary is {salary}");
+        decimal salary = employee.CalculateSalary();
+        Console.WriteLine($"The {employee.Fullname}'s salary is {salary}");
         
     }
 }
